723fb928d6da45e6cfaed1108149679dbfa74ac1
Andy Chen <andyc6074@hstat.org>
2023-04-09 19:24:46 -0400

    add my testing folder.

---
+++ andyTestFolder/js/andy.js
@@ -0,0 +1,67 @@
+var BootScene = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+    function BootScene ()
+    {
+        Phaser.Scene.call(this, { key: 'BootScene' });
+    },
+    preload: function ()
+    {
+        // load the resources here
+         // map tiles
+         this.load.image('tiles', 'assets/map/RPG Nature Tileset.png');
+
+         // map in json format
+         this.load.tilemapTiledJSON('map', 'assets/map/map3.json');
+
+         // our two characters
+         this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+    },
+    create: function ()
+    {
+        this.scene.start('WorldScene');
+    }
+});
+var WorldScene = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+    function WorldScene ()
+    {
+        Phaser.Scene.call(this, { key: 'WorldScene' });
+    },
+    preload: function ()
+    {
+
+    },
+    create: function ()
+    {
+        // create your world here
+        // var map = this.make.tilemap({ key: 'map' });
+        // var tiles = map.addTilesetImage('spritesheet', 'tiles');
+        // var grass = map.createStaticLayer('Grass', tiles, 0, 0);
+        var map = this.make.tilemap({key: 'map'});
+        var tiles = map.addTilesetImage ('RPG Nature Tileset','tiles');
+        var grass = map.createStaticLayer('Grass', 'tiles',22,32,0,0);
+        var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
+        // obstacles.setCollisionByExclusion([-1]);
+    }
+});
+var config = {
+    type: Phaser.AUTO,
+    parent: 'content',
+    width: 320,
+    height: 240,
+    zoom: 2,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
+    scene: [
+        BootScene,
+        WorldScene
+    ]
+};
+var game = new Phaser.Game(config);
